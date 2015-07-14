e7e79e1a9336abf43924a61a7ffcaec0a3a76c77
vipprokaka12 <nguyentannhut225@gmail.com>
2015-07-14 18:28:24 +0700

    Revert "Fist comiit"
    This reverts commit 92294a19fe08f281f3ee7f0d9542407373b890e6.

--- application/views/admin/control_login.php
+++
@@ -1,9 +0,0 @@
-
-<?php if($form_ms!=""){?>
-<div class="container-fluid">
-	<div class="alert alert-warning alert-danger" role="alert">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></button>
-      <strong>Warning!</strong><?php echo $form_ms ?>
-    </div>
-</div>
-<?php } ?>
